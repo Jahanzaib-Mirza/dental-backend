e9e3dfd78a67f6022d2c8d12240ab5a268cde84a
Muhammad Jahanzaib <jahanzaibm29@gmail.com>
2025-05-25 00:48:04 +0500

    production socket update

--- config/env/production.js
+++ config/env/production.js
@@ -265,10 +265,8 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: ["http://localhost:5173"]
+
 
 
     /***************************************************************************
