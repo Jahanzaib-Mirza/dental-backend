5e113634847bb61ff51dc8d37e6862ffacb7fce8
Muhammad Jahanzaib <jahanzaibm29@gmail.com>
2025-05-25 02:05:56 +0500

    Fixing access issue v5

--- config/security.js
+++ config/security.js
@@ -30,7 +30,7 @@ module.exports.security = {
 
   cors: {
     allRoutes: true,
-    allowOrigins: ['https://localhost:5173'],
+    allowOrigins: ['http://localhost:5173'],
     allowCredentials: true,
   },
 
